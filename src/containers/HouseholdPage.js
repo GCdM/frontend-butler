2c7df52609d6e27ec703a33a2127c1908e63d6bf
GCdM <gabrielcm.br@gmail.com>
2018-09-10 14:40:19 +0100

    Basic Calendar with nothing on it

--- src/containers/HouseholdPage.js
+++ src/containers/HouseholdPage.js
@@ -1,11 +1,17 @@
 import React from 'react'
-import { Container, Segment, Grid } from 'semantic-ui-react'
+import { Segment, Grid } from 'semantic-ui-react'
+import Calendar from 'react-calendar'
 
 import ExpenseForm from '../forms/ExpenseForm'
 import EventForm from '../forms/EventForm'
 import LeftHouseFeed from '../components/LeftHouseFeed'
 
 class HouseholdPage extends React.Component {
+  state = {
+    date: new Date(),
+  }
+
+  calendarChange = date => this.setState({ date })
 
   render() {
     return (
@@ -21,7 +27,11 @@ class HouseholdPage extends React.Component {
               <LeftHouseFeed/>
             </Grid.Column>
             <Grid.Column>
-
+              <Calendar
+                value={this.state.date}
+                onChange={this.calendarChange}
+                minDetail="year"
+              />
             </Grid.Column>
           </Grid.Row>
         </Grid>
